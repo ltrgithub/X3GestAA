912cd0de43aea785eb0753b446ede85224fc5ca6
walzm <mathias.walz@sage.de>
2012-07-13 15:49:22 +0200

    #67 Word interface WIP

---
+++ dotnet/WordAddIn/DatasourceForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WordAddIn
+{
+    public partial class DatasourceForm : Form
+    {
+        private SyracuseOfficeCustomData customData;
+
+        public DatasourceForm(SyracuseOfficeCustomData customData)
+        { 
+            this.customData = customData;
+            InitializeComponent(); 
+        }
+
+        private void DatasourceForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Uri uri = new Uri(customData.getServerUrl() + "/msoffice/lib/word/html/main.html?url=%3Frepresentation%3Dwordhome.%24dashboard");
+                browser.ObjectForScripting = new WordAddInJSExternal(customData, this);
+                browser.Url = uri;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
