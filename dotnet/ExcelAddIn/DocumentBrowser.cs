37987399be82018b31c90a2f7e26a35ed9ba67d0
Cristian Nitescu <cristi.nitescu@sage.com>
2012-11-08 10:32:53 +0100

    #335 - Hints for storage volume filter

--- dotnet/ExcelAddIn/DocumentBrowser.cs
+++ dotnet/ExcelAddIn/DocumentBrowser.cs
@@ -18,18 +18,22 @@ namespace ExcelAddIn
         //
         public void SelectDocument(String serverUrl, String volumeCode)
         {
+            string volumeUuid = "";
+            // GET THE VOLUME UUID
+            // make a GET http request to serverUrl + "/sdata/syracuse/collaboration/syracuse/storageVolumes?where=(code eq \"" + volumeCode + "\")"
+            // the result has a $resources array, volumeUuid is the $uuid property of the first element of the result
+            //
             webBrowser.Url = new Uri(serverUrl + "/msoffice/lib/excel/html/config.html?url=" + 
-                Uri.EscapeUriString("/sdata/syracuse/collaboration/syracuse/documents?representation=documentExcelSI.$lookup&volumeCode=" + volumeCode));
+                Uri.EscapeUriString("/sdata/syracuse/collaboration/syracuse/documents?representation=documentExcelSI.$lookup&where=(volume eq \"" + volumeUuid + "\")"));
             webBrowser.ObjectForScripting = new External();
-            // not sure we need to connect the action panel on this operation
-            /*((External)webBrowser.ObjectForScripting).onLogonHandler = delegate()
-            {
-                if (!Globals.ThisAddIn.ActionPanel.connected)
-                    Globals.ThisAddIn.ActionPanel.Connect("");
-            };*/
             ((External)webBrowser.ObjectForScripting).onSelectRecordHandler = delegate(string prototype, string dataset)
             {
+                // Test message
                 MessageBox.Show("Prototype: " + prototype + "; Dataset: " + dataset);
+                // GET THE CONTENT
+                // documentUuid is $uuid property of dataset
+                // deserialize prototype and dataset. 
+                // make a GET http request to serverUrl + "/sdata/syracuse/collaboration/syracuse/documents('" + documentUuid + "')/content"
                 Close();
             };
             webBrowser.Refresh();
