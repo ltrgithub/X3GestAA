766cedf1aa398981dd9159da205651ea4fdba2aa
walzm <mathias.walz@sage.de>
2013-03-13 15:44:55 +0100

    #836 also fixed refresh button statehandling

--- dotnet/WordAddIn/ReportingActions.cs
+++ dotnet/WordAddIn/ReportingActions.cs
@@ -67,7 +67,7 @@ namespace WordAddIn
                     Globals.Ribbons.Ribbon.buttonPreview.Enabled = true;
                     Globals.Ribbons.Ribbon.checkBoxShowTemplatePane.Enabled = true;
                 }
-                if (customData.getDocumentTemplateUrl() != null)
+                if (customData.getDocumentTemplateUrl() != null && rpt_fill_tpl.Equals(mode))
                 {
                     Globals.Ribbons.Ribbon.buttonRefreshReport.Enabled = true;
                 }
