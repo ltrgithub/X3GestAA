e70c19a078e0c40170402b9c418e50b3b8aa5011
walzm <mathias.walz@sage.de>
2013-04-25 10:21:41 +0200

    #1048 intercept V6 word document BLOBs in syracuse server and eventually respond with dummy document

--- dotnet/WordAddIn/WordAddInJSExternal.cs
+++ dotnet/WordAddIn/WordAddInJSExternal.cs
@@ -338,7 +338,7 @@ namespace WordAddIn
             return Globals.WordAddIn.commons.GetDocumentLocale(doc);
         }
 
-        public void v6Download(String layoutAndData)
+        public void v6Download()
         {
             Document doc = Globals.WordAddIn.getActiveDocument();
             SyracuseOfficeCustomData customData = SyracuseOfficeCustomData.getFromDocument(doc);
@@ -353,8 +353,14 @@ namespace WordAddIn
                     return;
                 }
 
+                string ext = ".doc";
+                if (content[0] == 0x50 && content[1] == 0x4b && content[2] == 0x03 && content[3] == 0x04)
+                {
+                    ext = "docx";
+                }
+
                 string newDocumentFile = null;
-                newDocumentFile = Path.GetTempFileName().Replace(".tmp" , ".doc");
+                newDocumentFile = Path.GetTempFileName().Replace(".tmp" , ext);
 
                 using (FileStream stream = new FileStream(newDocumentFile, FileMode.Create))
                 {
@@ -367,7 +373,10 @@ namespace WordAddIn
 
                 object doNotSaveChanges = WdSaveOptions.wdDoNotSaveChanges;
 
+                string tempFile = doc.FullName;
                 Globals.WordAddIn.Application.ActiveWindow.Close(ref doNotSaveChanges);
+                File.Delete(tempFile);
+
                 Globals.WordAddIn.Application.Documents.Open(newDocumentFile);
                 doc = Globals.WordAddIn.getActiveDocument();
                 if (doc == null)
