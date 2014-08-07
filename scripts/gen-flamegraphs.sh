a8a0ea0890d9332a79867d05bb539ab7a303b61c
Bruno Jouhier <bjouhier@gmail.com>
2014-08-07 18:55:57 +0200

    issue #4599 - added script

---
+++ scripts/gen-flamegraphs.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cat perf-recorded.data | node_modules/streamline/bin/_node --cache node_modules/streamline-flamegraph/lib/collapse  > perf-collapsed.data
+[ -f palette.map ] || echo "io->rgb(0,255,255)" > palette.map
+cat perf-collapsed.data | node_modules/streamline-flamegraph/deps/flamegraph.pl --cp  > perf-full.svg
+cat perf-collapsed.data | egrep -v -e ';io \d+$' | node_modules/streamline-flamegraph/deps/flamegraph.pl --cp > perf-cpu.svg
