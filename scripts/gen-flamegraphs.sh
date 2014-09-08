3c39c7a085e07cdae7357c6b2672796fcd394816
Bruno Jouhier <bjouhier@gmail.com>
2014-09-08 16:33:52 +0200

    issue #4599 - link to open source file from flamegraph

--- scripts/gen-flamegraphs.sh
+++ scripts/gen-flamegraphs.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-cat perf-recorded.data | node_modules/streamline/bin/_node --cache node_modules/streamline-flamegraph/lib/collapse  > perf-collapsed.data
-[ -f palette.map ] || echo "io->rgb(0,255,255)" > palette.map
-cat perf-collapsed.data | egrep -v -e '^node;pipe .*?;io \d+$' | node_modules/streamline-flamegraph/deps/flamegraph.pl --cp  > perf-full.svg
-cat perf-collapsed.data | egrep -v -e ';io \d+$' | node_modules/streamline-flamegraph/deps/flamegraph.pl --cp > perf-cpu.svg
+$(dirname $0)/../node_modules/streamline-flamegraph/bin/gen-graphs.sh
