9a1cca280973e617243e44c11d28fbb7f5cb1b61
Billy Girvan <William.Girvan@Sage.com>
2014-09-10 17:04:46 +0200

    #4605: adding unit testing project

---
+++ dotnet/UnitTests/LookupTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using CommonDataHelper.OwnerHelper;
+using CommonDataHelper;
+using System.Threading;
+using CommonDataHelper.StorageVolumeHelper;
+using CommonDataHelper.TagHelper;
+using CommonDataHelper.TeamHelper;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class LookupTests
+    {
+        [Test, RequiresSTA]
+        public void ownerLookupTest()
+        {
+            Assert.DoesNotThrow(ownerLookup);
+            Assert.DoesNotThrow(storageVolumeLookup);
+            Assert.DoesNotThrow(tagLookup);
+            Assert.DoesNotThrow(teamLookup);
+        }
+
+        [Test, RequiresSTA]
+        public void ownerLookup()
+        {
+            List<OwnerItem> list = new OwnerList().createOwnerList();
+            Assert.AreNotSame(null, list);
+            Assert.IsNotEmpty(list);
+        }
+
+        [Test, RequiresSTA]
+        public void storageVolumeLookup() 
+        {
+            List<StorageVolumeItem> list = new StorageVolumeList().createStorageVolumeList();
+            Assert.AreNotSame(null, list);
+            Assert.IsNotEmpty(list);
+        }
+
+        [Test, RequiresSTA]
+        public void tagLookup()
+        {
+            List<TagItem> list = new TagList().createTagList();
+            Assert.AreNotSame(null, list);
+            Assert.IsNotEmpty(list);
+        }
+
+        [Test, RequiresSTA]
+        public void teamLookup()
+        {
+            List<TeamItem> list = new TeamList().createTeamList();
+            Assert.AreNotSame(null, list);
+            Assert.IsNotEmpty(list);
+        }
+    }
+}
