28db8299fd7e2d9569711811370f68f62b7c8f25
frnana <franckkevin.nanademen@sage.com>
2015-02-12 15:54:51 +0100

    #5544 back to working version

--- phantomJS/screenshot.js
+++ phantomJS/screenshot.js
@@ -1,7 +1,16 @@
 var page = require('webpage').create();
-page.open('http://pc038043:8124', function(status) {
+var system = require('system');
+
+if (system.args.length < 2){
+	console.log('Usage: screenshot.js <some URL>');
+	phantom.exit();
+}
+
+var url = system.args[1];
+
+page.open(url, function(status) {
   console.log("Status: " + status);
-  if(status === "success") {
+  if (status === "success") {
     page.render('example.png');
   }
   phantom.exit();
