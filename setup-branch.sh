c35453c1326f2f7f60bb1b2331f664cdf2527cc5
Bruno Jouhier <bjouhier@gmail.com>
2015-08-21 14:21:14 +0200

    issue #6948 - improved setup-branch.sh script

--- setup-branch.sh
+++ setup-branch.sh
@@ -4,4 +4,16 @@ cd $DIR;
 git submodule init
 git submodule update
 cd $DIR/node_modules
-npm install babel-core streamline-plugin commander source-map
+for dir in ez-mongodb ez-streams galaxy jsurl streamline-doctool streamline-flamegraph streamline-fs \
+	streamline-pdfkit streamline-plugin streamline-require streamline-runtime; do
+		echo "git checkout in " $dir
+		cd $DIR/node_modules/$dir
+		git checkout .
+done;
+cd $DIR/node_modules
+npm install babel-core
+for dir in streamline-plugin commander source-map; do
+		echo "npm install in " $dir
+		cd $DIR/node_modules/$dir
+		npm install
+done;
