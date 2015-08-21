1d617b6fbd9bb51ef8aa918778f218135e367bf0
apisu <aurelien.pisu@sage.com>
2015-08-21 09:49:10 +0200

    #6968 modify implem from recursive to iteratif

--- nodelocal-template.js
+++ nodelocal-template.js
@@ -35,6 +35,8 @@ exports.config = {
 			dataset: "production",
 			landingPage: "http://localhost:8080/"
 		},*/
+		// allow to pass some node parameter like --prof to generate some log or other
+		nodeOptions : ""
 	},
 	system: {
 		// enables memwatch module
