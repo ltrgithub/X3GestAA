e93879443c2f41859c9e990db68b0910d1ba8547
Mathias Walz <mathias.walz@sage.com>
2015-09-04 09:15:31 +0200

    #6778 migrate native components project

---
+++ SageX3Mobile/WindowsUniversalApp/NativeComponents/Basics/NativeMessages.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Data.Xml.Dom;
+using Windows.Foundation.Metadata;
+using Windows.UI.Notifications;
+
+/// <summary>
+/// 
+/// </summary>
+namespace Sage.X3.Mobile.NativeComponents.Basics
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [AllowForWeb]
+    public sealed class NativeMessages
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="msgJson"></param>
+        public void ToastMsg(string msgJson)
+        {
+            JsonObject msgRoot = Windows.Data.Json.JsonValue.Parse(msgJson).GetObject();
+            string title = msgRoot.GetNamedString("title");
+            string body = msgRoot.GetNamedString("body");
+            string severity = msgRoot.GetNamedString("severity");
+
+            // <toast><visual><binding template=\"ToastImageAndText02\"><image id=\"1\" src=\"\"/><text id=\"1\"></text><text id=\"2\"></text></binding></visual></toast>
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText02;
+
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            XmlNodeList toastTextElements = toastXml.GetElementsByTagName("text");
+            toastTextElements[0].AppendChild(toastXml.CreateTextNode(title));
+            toastTextElements[1].AppendChild(toastXml.CreateTextNode(body));
+            IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            ((XmlElement)toastNode).SetAttribute("duration", "long");
+
+            ToastNotification toast = new ToastNotification(toastXml);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void UpdateTile()
+        {
+            XmlDocument tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare310x310ImageAndText01);
+            XmlNodeList tileTextAttributes = tileXml.GetElementsByTagName("text");
+            tileTextAttributes[0].AppendChild(tileXml.CreateTextNode("Hello World!"));
+            TileNotification tileNotification = new TileNotification(tileXml);
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+        }
+    }
+}
---
+++ SageX3Mobile/WindowsUniversalApp/NativeComponents/Basics/NativeSageX3WUPApp.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+/// <summary>
+/// 
+/// </summary>
+namespace Sage.X3.Mobile.NativeComponents.Basics
+{
+    public interface INativeSageX3WUPApp
+    {
+        void ConfigServer();
+        void NotifLoaded();
+        void NotifStartFail(string msg);
+    }
+
+    /// <summary>
+    /// Class to directly allow interaction with the wrapper and internal webapp
+    /// This is a wrapper class with no external dependencies so it can be shared with the webview
+    /// It only delegates all calls to the interface given on construction
+    /// </summary>
+    [AllowForWeb]
+    public sealed class NativeSageX3WUPAppWrapper : INativeSageX3WUPApp
+    {
+        private INativeSageX3WUPApp impl;
+
+        public NativeSageX3WUPAppWrapper(INativeSageX3WUPApp impl)
+        {
+            this.impl = impl;
+        }
+
+        public void ConfigServer()
+        {
+            this.impl.ConfigServer();
+        }
+
+        public void NotifLoaded()
+        {
+            this.impl.NotifLoaded();
+        }
+
+        public void NotifStartFail(string msg)
+        {
+            this.impl.NotifStartFail(msg);
+        }
+    }
+}
