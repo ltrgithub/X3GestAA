120f558f70aa7f27ef8c3a7ff91d0f228afa8514
Billy Girvan <William.Girvan@Sage.com>
2016-03-08 15:06:55 +0100

    #8245: enabling Office 2016 support in u9 Outlook add-in

--- dotnet/OutlookAddIn/InstallerHelpers/RegistryHelper/Program.cs
+++ dotnet/OutlookAddIn/InstallerHelpers/RegistryHelper/Program.cs
@@ -1,24 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Windows.Forms;
 
 namespace RegistryHelper
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            //String installDirectory = @"C:\Program Files (x86)\Sage\Sage ERP X3 Outlook AddIn".Trim(System.IO.Path.GetInvalidPathChars());
-            String installDirectory = args[0].Trim(System.IO.Path.GetInvalidPathChars());
-
-            if (Office2010RegistryHelper.isOffice2010Installed())
-            {
-                Office2010RegistryHelper.registerAddIn(installDirectory);
-            }
-            else if (Office2013RegistryHelper.isOffice2013Installed())
-            {
-                Office2013RegistryHelper.registerAddIn(installDirectory);
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UpdateRegistryForm());
         }
     }
 }
