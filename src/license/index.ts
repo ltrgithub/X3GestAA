322a14b006e470e2823e931a5d4db65807d0ab72
Bruno Jouhier <bjouhier@gmail.com>
2016-11-18 13:54:15 +0100

    issue #9295 - testing with local license binaries

--- src/license/index.ts
+++ src/license/index.ts
@@ -32,7 +32,9 @@ if (config.noDevLic) {
 exports.LICENSE_FILE = LICENSE_FILE;
 
 exports.load = function(name) {
-	return require('../build/Release/' + name);
+	var arch = os.platform() + '-' + os.arch();
+	var v8 = 'v8-' + /[0-9]+\.[0-9]+/.exec(process.versions.v8)[0];
+	return require(`../bin/${arch}-${v8}/${name}`);
 };
 
 // add policies from policies folder
