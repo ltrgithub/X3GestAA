e6326c24b68fc9f62caa430813441b20a1a9761e
Eric Boureau <eric.boureau@sage.com>
2017-02-13 18:20:57 +0100

    Revert "#24485 Clean code"
    This reverts commit 0803ceef3b71b680c2e3113734611f5096a8e272.

--- nodelocal-template.js
+++ nodelocal-template.js
@@ -274,7 +274,7 @@ exports.config = {
 		// ignoreFrequency: true,
 		// offStemmer : true, // desactivation of the stemmer for the search indexation
         // useFolderNameAsIndexName: false, // for X3 instead of dataset, use solutionName.folderName as index name
-		//indexPrefix : "tenantXXX_configYYY" // used by example by cloudV2 to prefix index names in single tenant mode
+		//indexCloudPrefix : "tenantXXX_configYYY" // used by cloudV2 to prefix index names in single tenant mode
 	},
 	notificatonServer: {
 		//"log Level" : 3,
