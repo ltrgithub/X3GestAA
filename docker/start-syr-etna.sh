c44dad14c2fc14c668c98f245ed9d6f446f128f7
Cristian Nitescu <cristi.nitescu@sage.com>
2017-04-11 11:13:58 +0200

    #9966 - Typo in startup script

--- docker/start-syr-etna.sh
+++ docker/start-syr-etna.sh
@@ -1,9 +1,9 @@
 if [ "$(ls -A /config/nodelocal.js)" ]; then
   cp -f /config/nodelocal.js .
 fi
-if [ "$(ls -A /config/devLicence.json)" ]; then
+if [ "$(ls -A /config/devLicense.json)" ]; then
   mkdir -p devLic
-  cp -f /config/devLicence.json ./devLic/license.json
+  cp -f /config/devLicense.json ./devLic/license.json
 fi
 
 node .
